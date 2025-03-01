abc3179b99874d66de7cb6d5afa5b8e2ceda99ad
Max Blomstervall <max.blomstervall@plejd.com>
2025-03-01 22:06:37 +0100

    add formatting for javascript, jsreact, typescript and typescript react

--- lua/myLuaConf/format/init.lua
+++ lua/myLuaConf/format/init.lua
@@ -22,7 +22,10 @@ require('lze').load {
           -- Conform will run multiple formatters sequentially
           -- python = { "isort", "black" },
           -- Use a sub-list to run only the first available formatter
-          -- javascript = { { "prettierd", "prettier" } },
+          javascript = { "prettierd", "prettier", stop_after_first = true },
+          javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+          typescript = { "prettierd", "prettier", stop_after_first = true },
+          typescriptreact = { "prettierd", "prettier", stop_after_first = true },
         },
       })
 
