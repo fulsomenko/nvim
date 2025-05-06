2fd115049ff3615a73f8c1ffafa491a2da43a974
Max Blomstervall <max.blomstervall@proposales.com>
2025-05-07 00:34:41 +0200

    add toggle bind for nerdtree

--- lua/myLuaConf/plugins/nerdtree.lua
+++ lua/myLuaConf/plugins/nerdtree.lua
@@ -7,6 +7,20 @@ return {
       desc = "Focus NERDTree",
       mode = { "n", "v" },
     },
-  }
+    {
+      "<leader>t",
+      ":NERDTreeToggle<CR>",
+      desc = "Toggle NERDTree",
+      mode = { "n", "v" },
+    },
+  },
+  after = function()
+    -- Optional: Set global variables or run commands after loading NERDTree
+    vim.g.NERDTreeShowHidden = 1
+    -- Example: Open NERDTree if no files are specified on startup
+    vim.cmd([[
+      autocmd VimEnter * if !argc() | NERDTree | endif
+    ]])
+  end,
 }
 
